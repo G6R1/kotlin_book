fe521bfb812e7f49315310d6bed8797b3b0add60
Lev Durov <gerian109@yandex.ru>
2023-09-28 00:34:18 +0300

    str 50

---
+++ inner_project_with_gradle/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+const val HERO_NAME = "Mordred"
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var playerLevel = 4
+    val req = 7
+    println(playerLevel)
+    playerLevel += 5
+    println(playerLevel)
+    println("$HERO_NAME, son of Morgana")
+}
